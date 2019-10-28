b026c7555c2226a2cf5cdc2b85c0fc242583c080
ishamunavvar <ishamunavvar@invite.com>
2019-10-28 14:25:12 -0500

    media

--- Invitation/assets/js/app.js
+++ Invitation/assets/js/app.js
@@ -192,3 +192,30 @@ let countDown = new Date('Dec 17, 2019 00:00:00').getTime(),
       //}
 
     }, second)
+
+  // php FormData
+
+
+  function myFunction() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var contact = document.getElementById("contact").value;
+    // Returns successful data submission message when the entered information is stored in database.
+    var dataString = 'name1=' + name + '&email1=' + email + '&password1=' + password + '&contact1=' + contact;
+    if (name == '' || email == '' || password == '' || contact == '') {
+    alert("Please Fill All Fields");
+    } else {
+    // AJAX code to submit form.
+    $.ajax({
+    type: "POST",
+    url: "form.php",
+    data: dataString,
+    cache: false,
+    success: function(html) {
+    alert(html);
+    }
+    });
+    }
+    return false;
+    }
